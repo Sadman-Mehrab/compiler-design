c55cd0be6ff9a2c749ca085881798e063dd77324
Sadman Mehrab <sadmannaqueeb@gmail.com>
2024-02-06 20:29:19 +0600

    6/2/24 fix one.cpp

--- Lab2/one.cpp
+++ Lab2/one.cpp
@@ -9,14 +9,22 @@ int main () {
     bool isNumeric = true;
     bool seenMinus = false;
     bool seenPoint = false;
+    
     for (int i=0;i<s.length();i++){
 
-        if( !(s[i] >= '0' && s[i] <= '9')){
-            if( s[i] == '.' || s[i] == '-' ) isNumeric = true;
-            else isNumeric = false;
+        if( !(s[i] >= '0' && s[i] <= '9') ){
+            if(s[i] == '.'){
+                if(!seenPoint) seenPoint = true;
+                else isNumeric = false;
+            }
+            else if(s[i] == '-'){
+                if(i == 0) seenMinus = true;
+                else if(seenMinus) isNumeric = false;
+            }
+            else if(s[i] != '.' && s[i] != '-') isNumeric = false;
         }
-
     }
+
     if (isNumeric) cout<<"Numeric Constant\n";
     else cout<<"Not Numeric\n";
 
